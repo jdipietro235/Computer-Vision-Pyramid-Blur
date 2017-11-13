d3eaddaea79001c881f2dd7a0eb4f75edad95790
EagleBuck <jdipietro235@gmail.com>
2017-11-13 04:13:17 -0500

    continuing to work on new file

--- code/restartPyr.py
+++ code/restartPyr.py
@@ -9,7 +9,12 @@
 #   input is image
 #   output is lapl and gaus pyramids
 
-
+import numpy as np
+import scipy
+import scipy.signal as sig
+from scipy import misc
+import matplotlib.pyplot as plt
+from scipy import ndimage
 
 def main():
     while(True):
@@ -27,6 +32,15 @@ def main():
             break
         except(IOError):
             print('invalid name')
+
+    gausA, laplPyrA = pyramids(imageA)
+    gausB, laplPyrB = pyramids(imageB)
+
+    rowCountA, colCountA = imageA.shape
+    rowCountB, colCountB = imageB.shape
+
+    compositeImage = np.zeros((rowCountA, colCountA + colCountA / 2), dtype=np.double)
+    compositeImage[:rowCountA, :colCountA] = gausA[0]
 
 
 
@@ -47,7 +61,21 @@ def interpolate(image, kernel, rate):
     return output
 
 
+def pyramids(image, kernel, rate):
+    gausPyr = [image, ]
+    laplPyr = []
 
+    modImage = image
+
+    while modImage.shape[0] >= 2 and modImage.shape[1] >= 2:
+        modImage = decimate(modImage)
+        gausPyr.append(modImage)
+
+    for i in range(len(GausPyr) - 1):
+        laplPyr.append(gausPyr[i] - interpolate(gaus[i + 1]))
+        #the lapl level is a level of gaus minus the next level up
+
+    return gausPyr[:-1], laplPyr
 
 
 
